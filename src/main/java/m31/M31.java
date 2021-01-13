5736a194a45503e288d890748794edd2c108a3f1
lunarsmile <77017099+lunarsmile@users.noreply.github.com>
2021-01-13 23:48:25 +0800

    Add start() and stop() in M31
      In start(), initialize to easy to bootstrap a channel to use as a client
      In stop(), shut down the worker event loop if necessary

---
+++ src/main/java/m31/M31.java
@@ -0,0 +1,37 @@
+package m31;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+public class M31 {
+
+  private EventLoopGroup worker;
+  private Bootstrap bs;
+
+  public void start() {
+    worker = new NioEventLoopGroup();
+
+    bs = new Bootstrap();
+    bs.group(worker)
+      .channel(NioSocketChannel.class)
+      .handler(new ChannelInitializer<SocketChannel>() {
+        @Override
+        protected void initChannel(SocketChannel ch) {
+          ch.pipeline()
+            .addLast(new LoggingHandler(LogLevel.TRACE));
+        }
+      });
+  }
+
+  public void stop() {
+    if (worker != null) {
+      worker.shutdownGracefully();
+    }
+  }
+}
