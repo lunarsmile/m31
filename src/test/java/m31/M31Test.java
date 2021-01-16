cd83eba1cec842056b1a24fe49afa7da2f294b7c
lunarsmile <77017099+lunarsmile@users.noreply.github.com>
2021-01-16 23:59:07 +0800

    Use M31d in M31Test as server to test basic connection

--- src/test/java/m31/M31Test.java
+++ src/test/java/m31/M31Test.java
@@ -3,23 +3,24 @@ package m31;
 import m31.common.Session;
 import org.junit.Test;
 
-import java.net.InetSocketAddress;
-import java.nio.channels.AsynchronousServerSocketChannel;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
 public class M31Test {
 
   @Test
-  public void testNormalConnection() throws Exception {
-    try (AsynchronousServerSocketChannel srv = AsynchronousServerSocketChannel.open()) {
-      srv.bind(new InetSocketAddress("127.0.0.1", 12345));
-      srv.accept();
+  public void testNormalConnection() {
+    try(M31d m31d = new M31d()) {
+      m31d.open(12345);
 
       M31 client = new M31();
       client.start();
-      try (Session session = client.connect("127.0.0.1", 12345).get()) {
+      try (Session session = client.connect("127.0.0.1", 12345).get(1, TimeUnit.SECONDS)) {
         assertNotNull(session);
+      } catch (Exception e) {
+        fail("Unable to connect");
       }
       client.stop();
     }
