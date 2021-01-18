b9410e79563414e4a0582ce6f508acdb3d20a571
lunarsmile <77017099+lunarsmile@users.noreply.github.com>
2021-01-19 00:05:17 +0800

    Add a new handler interface for ID exchange
      Introduce a new interface - IdExHandler
      Add some test cases for IdExHandler.getId()

---
+++ src/main/java/m31/common/IdExHandler.java
@@ -0,0 +1,104 @@
+package m31.common;
+
+import io.netty.buffer.ByteBuf;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+
+public interface IdExHandler {
+
+  /**
+   * The maximum length of the string is 255 characters, including the Carriage Return and Line Feed.
+   */
+  int MAX_IDENTIFICATION_LINE_LENGTH = 255;
+
+  /**
+   * Get the remote peer's identification.
+   *
+   * <p>
+   *   This method parse the content of {@link ByteBuf} parameter to see if it match the
+   *   identification patterns, which:
+   *   <ul>
+   *     <li>either starts with "C-" or "S-"</li>
+   *     <li>ends with "\r\n"</li>
+   *   </ul>
+   * </p>
+   * <p>
+   *   If the buffer contains multiple lines - string holds one or multiple "\r\n", only the first
+   *   line that matches the patterns above would be returned, other lines would be ignored.
+   * </p>
+   * <p>
+   *   This method is intended to traversal each character inside the buffer at most once, any more
+   *   effort spent more than that should be redundant.
+   * </p>
+   *
+   * @param buf The {@link ByteBuf} buffer where the ID to be parsed from
+   * @return The client or server ID as a string
+   */
+  static String getId(ByteBuf buf) {
+    Objects.requireNonNull(buf, "Parameter cannot be null");
+
+    int rIdx = buf.readerIndex();
+    int wIdx = buf.writerIndex();
+    if (rIdx == wIdx) {
+      return null;
+    }
+
+    int line = 1, pos = 0;
+    boolean needLf = false;
+    boolean validLine = false;
+
+    byte[] data = new byte[MAX_IDENTIFICATION_LINE_LENGTH];
+
+    rIdx--;
+    while (rIdx++ < wIdx) {
+      byte b = buf.getByte(rIdx);
+
+      if (b == '\0') {
+        throw new IllegalStateException("Illegal identification - null character found at" +
+            " line #" + line + " character #" + (pos + 1));
+      }
+
+      if (b == '\r') {
+        needLf = true;
+
+        continue;
+      }
+
+      if (b == '\n') {
+        line++;
+
+        if (validLine) {
+          buf.readerIndex(rIdx + 1);
+          buf.discardReadBytes();
+
+          return new String(data, 0, pos, StandardCharsets.UTF_8);
+        }
+
+        pos = 0;
+        needLf = false;
+
+        continue;
+      }
+
+      if (needLf) {
+        throw new IllegalStateException("Illegal identification - invalid line ending at" +
+            " line #" + line + " character #" + pos + 1);
+      }
+
+      if (pos > data.length) {
+        throw new IllegalStateException("Illegal identification - line too long at line #" + line +
+            " character #" + pos + 1);
+      }
+
+      if (pos < 2) {
+        data[pos++] = b;
+      } else if ((data[0] == 'C' && data[1] == '-') || (data[0] == 'S' && data[1] == '-')) {
+        validLine = true;
+        data[pos++] = b;
+      }
+    }
+
+    return null;
+  }
+}
---
+++ src/test/java/m31/common/IdExHandlerTest.java
@@ -0,0 +1,60 @@
+package m31.common;
+
+import io.netty.buffer.Unpooled;
+import org.junit.Test;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.Assert.*;
+
+public class IdExHandlerTest {
+
+  private final Charset utf8 = StandardCharsets.UTF_8;
+
+  @Test
+  public void testGetIdSingleLine() {
+    String id = "C-2.0-softwareversion\r\n";
+    String actual = IdExHandler.getId(Unpooled.wrappedBuffer(id.getBytes(utf8)));
+    assertEquals("C-2.0-softwareversion", actual);
+
+    id = "S-2.0-softwareversion\r\n";
+    actual = IdExHandler.getId(Unpooled.wrappedBuffer(id.getBytes(utf8)));
+    assertEquals("S-2.0-softwareversion", actual);
+  }
+
+  @Test
+  public void testGetIdSingleLineWithoutCR() {
+    String id = "C-2.0-softwareversion\n";
+    String actual = IdExHandler.getId(Unpooled.wrappedBuffer(id.getBytes(utf8)));
+    assertEquals("C-2.0-softwareversion", actual);
+
+    id = "S-2.0-softwareversion\n";
+    actual = IdExHandler.getId(Unpooled.wrappedBuffer(id.getBytes(utf8)));
+    assertEquals("S-2.0-softwareversion", actual);
+  }
+
+  @Test
+  public void testGetIdContainsNullCharacter() {
+    String id = "C-2.0-so" + '\0' + "ftwareversion\r\n";
+
+    IllegalStateException thrown =
+        assertThrows(IllegalStateException.class, () -> {
+          IdExHandler.getId(Unpooled.wrappedBuffer(id.getBytes(utf8)));
+        });
+
+    assertNotNull(thrown.getMessage());
+    assertTrue(thrown.getMessage().contains("character #" + (id.indexOf('\0') + 1)));
+  }
+
+  @Test
+  public void testGetIdMultipleLines() {
+    String id = "1st line\r\nC-2.0-softwareversion\r\n";
+    String actual = IdExHandler.getId(Unpooled.wrappedBuffer(id.getBytes(utf8)));
+    assertEquals("C-2.0-softwareversion", actual);
+
+    id = "1st line\r\nS-2.0-softwareversion\r\n";
+    actual = IdExHandler.getId(Unpooled.wrappedBuffer(id.getBytes(utf8)));
+    assertEquals("S-2.0-softwareversion", actual);
+  }
+}
